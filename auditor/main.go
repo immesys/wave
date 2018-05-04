bfbb8ba1f774f1178a095f1dbe8825592aae851f
Michael Andersen <m.andersen@cs.berkeley.edu>
2018-05-04 15:04:20 -0700

    Finish auditor for paper

--- auditor/main.go
+++ auditor/main.go
@@ -11,31 +11,28 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/BurntSushi/toml"
 	"github.com/gogo/protobuf/proto"
 	"github.com/golang/glog"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/any"
 	"github.com/google/trillian"
 	"github.com/google/trillian/client"
+	tcrypto "github.com/google/trillian/crypto"
 	"github.com/google/trillian/crypto/keys/der"
 	"github.com/google/trillian/crypto/keyspb"
 	spb "github.com/google/trillian/crypto/sigpb"
 	"github.com/google/trillian/merkle/hashers"
 	_ "github.com/google/trillian/merkle/rfc6962"
 	"github.com/google/trillian/types"
+	"github.com/gorilla/pat"
 	"github.com/immesys/wave/storage/simplehttp"
 	"github.com/immesys/wave/storage/vldmstorage2/pb"
 	"google.golang.org/grpc"
 )
-
-const location = "http://127.0.0.1:8080/v1"
-const PublicKey = `-----BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEWs0VFmqFr27SeEw4DttPmglDhqMj
-KNzHzySfKQi6/IE4AlBvPlJOqEawPXVr3gJlk8SWlt3Ts9MEWFaLKc4lAQ==
------END PUBLIC KEY-----`
 
 var logverifier *client.LogVerifier
 
@@ -116,9 +113,42 @@ var maprootindex int64
 var oplogSTH *trillian.SignedLogRoot
 var maplogSTH *trillian.SignedLogRoot
 
+var signatures map[string]bool
+var sigmu sync.Mutex
+
+type Config struct {
+	Target         string
+	PublicKey      string
+	Output         string
+	Peers          []string
+	PeerListenPort int
+}
+
+var conf *Config
+var ofile *os.File
+
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("usage: ./auditor <config.toml>\n")
+		os.Exit(1)
+	}
+	cfg, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Printf("couldn't read config: %v\n", err)
+		os.Exit(1)
+	}
+	if _, err := toml.Decode(string(cfg), &conf); err != nil {
+		fmt.Printf("couldn't read config: %v\n", err)
+		os.Exit(1)
+	}
+	ofile, err = os.Create(conf.Output)
+	if err != nil {
+		fmt.Printf("could not open output file: %v\n", err)
+		os.Exit(1)
+	}
 	initmap()
-	pubk, _ := pem.Decode([]byte(PublicKey))
+	signatures = make(map[string]bool)
+	pubk, _ := pem.Decode([]byte(conf.PublicKey))
 	pubkobj, err := der.UnmarshalPublicKey(pubk.Bytes)
 	if err != nil {
 		panic(err)
@@ -130,26 +160,64 @@ func main() {
 	logverifier = client.NewLogVerifier(hasher, pubkobj, crypto.SHA256)
 	oplogSTH = getLogSTH("oplog")
 	maplogSTH = getLogSTH("rootlog")
-
+	if oplogSTH == nil || maplogSTH == nil {
+		fmt.Printf("could not get initial STHs\n")
+		os.Exit(1)
+	}
+	go StartHTTP()
 	for {
 		time.Sleep(10 * time.Second)
 		newOpLogSTH := getLogSTH("oplog")
-		if newOpLogSTH.TreeSize <= oplogSTH.TreeSize {
+		if newOpLogSTH == nil || newOpLogSTH.TreeSize <= oplogSTH.TreeSize {
 			continue
 		}
 		newMapLogSTH := getLogSTH("rootlog")
-		auditLog("oplog", oplogSTH, newOpLogSTH)
-		auditLog("rootlog", maplogSTH, newMapLogSTH)
-		verifyMap()
+		if newMapLogSTH == nil {
+			continue
+		}
+		if maplogSTH.TreeSize != 0 {
+			auditLog("oplog", oplogSTH, newOpLogSTH)
+			auditLog("rootlog", maplogSTH, newMapLogSTH)
+			verifyMap()
+		}
 		oplogSTH = newOpLogSTH
 		maplogSTH = newMapLogSTH
+		sigmu.Lock()
+		signatures[string(maplogSTH.LogRootSignature)] = true
+		sigmu.Unlock()
+		gossip(maplogSTH)
 	}
 }
 
-func get(suffix string) []byte {
-	response, err := http.Get(location + suffix)
+func gossip(sth *trillian.SignedLogRoot) {
+	ba, err := proto.Marshal(sth)
 	if err != nil {
 		panic(err)
+	}
+	for _, peer := range conf.Peers {
+		b := bytes.NewBuffer(ba)
+		req, err := http.NewRequest("POST", peer+"/v1/sth", b)
+		if err != nil {
+			panic(err)
+		}
+
+		ctx, cancel := context.WithTimeout(req.Context(), 500*time.Millisecond)
+		defer cancel()
+
+		req = req.WithContext(ctx)
+
+		client := http.DefaultClient
+		_, err = client.Do(req)
+		if err != nil {
+			fmt.Printf("PEER ERROR %v\n", err)
+		}
+	}
+}
+func get(suffix string) ([]byte, bool) {
+	response, err := http.Get(conf.Target + suffix)
+	if err != nil {
+		fmt.Printf("got HTTP error: %v\n", err)
+		return nil, false
 	}
 	defer response.Body.Close()
 
@@ -159,12 +227,15 @@ func get(suffix string) []byte {
 	}
 	if response.StatusCode != 200 {
 		fmt.Printf("got non 200: %v\n", string(contents))
-		panic("abort")
+		return nil, false
 	}
-	return contents
+	return contents, true
 }
 func getLogSTH(log string) *trillian.SignedLogRoot {
-	ba := get("/audit/" + log + "/sth")
+	ba, ok := get("/audit/" + log + "/sth")
+	if !ok {
+		return nil
+	}
 	smr := &trillian.SignedLogRoot{}
 	err := proto.Unmarshal(ba, smr)
 	if err != nil {
@@ -174,20 +245,23 @@ func getLogSTH(log string) *trillian.SignedLogRoot {
 	return smr
 }
 func auditLog(log string, from *trillian.SignedLogRoot, to *trillian.SignedLogRoot) bool {
-	ba := get(fmt.Sprintf("/audit/"+log+"/consistency?from=%d&to=%d", from.TreeSize, to.TreeSize))
+	ba, ok := get(fmt.Sprintf("/audit/"+log+"/consistency?from=%d&to=%d", from.TreeSize, to.TreeSize))
+	if !ok {
+		report("log failed to answer HTTP request")
+	}
 	proof := &trillian.Proof{}
 	err := proto.Unmarshal(ba, proof)
 	if err != nil {
-		panic(err)
+		report("invalid consistency proof (unmarshal)", ba)
 	}
 	lrv1 := &types.LogRootV1{}
 	err = lrv1.UnmarshalBinary(from.LogRoot)
 	if err != nil {
-		panic(err)
+		report("invalid consistency proof (logroot)", ba)
 	}
 	_, err = logverifier.VerifyRoot(lrv1, to, proof.Hashes)
 	if err != nil {
-		panic(err)
+		report("root failed to verify", ba)
 	}
 	return true
 }
@@ -198,13 +272,75 @@ type PromiseObject struct {
 	Value   []byte
 }
 
+func report(msg string, proof ...[]byte) {
+	ofile.Write([]byte(fmt.Sprintf("ERROR %s: %s\n", time.Now().Format(time.RFC3339), msg)))
+	for idx, p := range proof {
+		ofile.Write([]byte(fmt.Sprintf(" %d: %x\n", idx, p)))
+	}
+}
+func StartHTTP() {
+	r := pat.New()
+	r.Post("/v1/sth", PeerSTHHandler)
+	http.Handle("/", r)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", conf.PeerListenPort), nil)
+	panic(err)
+}
+
+func PeerSTHHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	ba, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+
+	slr := &trillian.SignedLogRoot{}
+	err = proto.Unmarshal(ba, slr)
+	if err != nil {
+		fmt.Printf("peer unmarshal error\n")
+		return
+	}
+
+	sigmu.Lock()
+	seen := signatures[string(slr.LogRootSignature)]
+	sigmu.Unlock()
+	if seen {
+		fmt.Printf("peer sent STH we have seen\n")
+		return
+	}
+	//Verify it to make sure its the same key
+	_, err = tcrypto.VerifySignedLogRoot(logverifier.PubKey, logverifier.SigHash, slr)
+	if err != nil {
+		fmt.Printf("peer sent bad log root\n")
+		return
+	}
+	//Get inclusion proof for this root
+	curSTH := maplogSTH
+	if slr.TreeSize < curSTH.TreeSize {
+		fmt.Printf("peer STH is before ours\n")
+		auditLog("rootlog", slr, curSTH)
+	} else if slr.TreeSize > curSTH.TreeSize {
+		fmt.Printf("peer STH is after ours\n")
+		auditLog("rootlog", curSTH, slr)
+		sigmu.Lock()
+		signatures[string(slr.LogRootSignature)] = true
+		sigmu.Unlock()
+	} else {
+		current, _ := proto.Marshal(curSTH)
+		report("Two STH's found with the same tree size", ba, current)
+	}
+}
+
 func verifyMap() {
 	//get the map root
 	for {
 		if maprootindex >= maplogSTH.TreeSize {
 			return
 		}
-		ba := get(fmt.Sprintf("/audit/rootlog/item?index=%d&size=%d", maprootindex, maplogSTH.TreeSize))
+		ba, ok := get(fmt.Sprintf("/audit/rootlog/item?index=%d&size=%d", maprootindex, maplogSTH.TreeSize))
+		if !ok {
+			report("log failed to answer item query")
+			return
+		}
 		ger := &trillian.GetEntryAndProofResponse{}
 		err := proto.Unmarshal(ba, ger)
 		//TODO check proof
@@ -216,25 +352,26 @@ func verifyMap() {
 		if err != nil {
 			panic(err)
 		}
-		spew.Dump(smr)
 		mrv1 := &types.MapRootV1{}
 		err = mrv1.UnmarshalBinary(smr.MapRoot)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("target map of size %d has hash %x\n", mrv1.Revision, mrv1.RootHash)
 		mapperMetadata := &pb.MapperMetadata{}
 		if err := proto.Unmarshal(mrv1.Metadata, mapperMetadata); err != nil {
 			panic(err)
 		}
 		ops := []*trillian.MapLeaf{}
 		for i := currentMapOpLogIndex; i <= mapperMetadata.HighestFullyCompletedSeq; i++ {
-			fmt.Printf("getting index %d\n", i)
-			ba := get(fmt.Sprintf("/audit/oplog/item?index=%d&size=%d", i, oplogSTH.TreeSize))
+			ba, ok := get(fmt.Sprintf("/audit/oplog/item?index=%d&size=%d", i, oplogSTH.TreeSize))
+			if !ok {
+				report("log failed to answer item query")
+				return
+			}
 			rsp := &trillian.GetEntryAndProofResponse{}
 			err := proto.Unmarshal(ba, rsp)
 			if err != nil {
-				panic(err)
+				report("object failed to unmarshal", ba)
 			}
 			lrv1 := &types.LogRootV1{}
 			err = lrv1.UnmarshalBinary(oplogSTH.LogRoot)
@@ -244,12 +381,13 @@ func verifyMap() {
 
 			err = logverifier.VerifyInclusionAtIndex(lrv1, rsp.Leaf.LeafValue, rsp.Leaf.LeafIndex, rsp.Proof.Hashes)
 			if err != nil {
-				panic(err)
+				report("inclusion proof failed to validate", ba)
+				return
 			}
 			mp := &PromiseObject{}
 			err = json.Unmarshal(rsp.Leaf.LeafValue, &mp)
 			if err != nil {
-				panic(err)
+				report("object failed to unmarshal", ba)
 			}
 			ops = append(ops, &trillian.MapLeaf{
 				Index:     mp.Key,
@@ -257,13 +395,11 @@ func verifyMap() {
 			})
 		}
 		currentMapOpLogIndex = mapperMetadata.HighestFullyCompletedSeq + 1
-		fmt.Printf("appling %d operations\n", len(ops))
 		req := &trillian.SetMapLeavesRequest{
 			MapId:    mapId,
 			Leaves:   ops,
 			Metadata: mrv1.Metadata,
 		}
-		spew.Dump(req)
 		resp, err := vmap.SetLeaves(context.Background(), req)
 		if err != nil {
 			panic(err)
@@ -273,13 +409,11 @@ func verifyMap() {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("our map of size %d has hash %x\n", replicamaproot.Revision, replicamaproot.RootHash)
 		if bytes.Equal(replicamaproot.RootHash, mrv1.RootHash) {
-			fmt.Printf("MAP ROOT VALIDATED SUCCESSFULLY\n")
+			fmt.Printf("MAP ROOT %d VALIDATED SUCCESSFULLY\n", maprootindex)
 			maprootindex++
 		} else {
-			fmt.Printf("MAP ROOT FAILED TO VALIDATE\n")
-			os.Exit(1)
+			report("map root failed to validate", ba)
 		}
 	}
 }
